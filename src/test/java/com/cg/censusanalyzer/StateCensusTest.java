3dc351ab5dafec46be72593eb1f337bec85250c9
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-27 01:12:12 +0530

    State census data

--- src/test/java/com/cg/censusanalyzer/StateCensusTest.java
+++ src/test/java/com/cg/censusanalyzer/StateCensusTest.java
@@ -6,49 +6,15 @@ import org.junit.Test;
 import java.io.IOException;
 
 public class StateCensusTest {
-    StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
-    @Test
-    public void when_ReadCSVFile_Count_Records_Should_Return_True() {
-        try {
-            Assert.assertEquals(37, stateCensusAnalyser.getCountStateRecords("/Users/abhinavthakur/Desktop/Intellij/IndianStatesCensusAnalyzer/StateCode.csv","com.cg.censusanalyzer.State"));
-        } catch (CustomException e) {
-            System.out.println(e.getMessage());
-            Assert.assertEquals(CustomException.ExceptionType.FILE_NOT_FOUND, e.type);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    @Test
-    public void when_InCorrectCSV_FileType_Should_Return_False() throws IOException {
-        try {
-            Assert.assertEquals(37, stateCensusAnalyser.getCountStateRecords("/Users/abhinavthakur/Desktop/Intellij/IndianStatesCensusAnalyzer/StateCode.csv","com.cg.censusanalyzer.State"));
-        } catch (CustomException e) {
-            System.out.println(e.getMessage());
-            Assert.assertEquals(CustomException.ExceptionType.BINDING_PROBLEM_AT_RUNTIME, e.type);
-        }
-
-    }
-    @Test
-    public void when_CorrectCSVFile_But_Delimiter_Incorrect_Should_Return1_False() throws IOException {
-        try {
-            Assert.assertEquals(37, stateCensusAnalyser.getCountStateRecords("/Users/abhinavthakur/Desktop/Intellij/IndianStatesCensusAnalyzer/StateCode.csv","com.cg.censusanalyzer.State"));
-        } catch (CustomException e) {
-            System.out.println(e.getMessage());
-            Assert.assertEquals(CustomException.ExceptionType.BINDING_PROBLEM_AT_RUNTIME, e.type);
-        }
-
-    }
-    @Test
-    public void when_CorrectCSVFile_But_Header_Incorrect_Should_Return_False() throws IOException {
-        try {
-            Assert.assertEquals(37, stateCensusAnalyser.getCountStateRecords("/Users/abhinavthakur/Desktop/Intellij/IndianStatesCensusAnalyzer/StateCode.csv","com.cg.censusanalyzer.State"));
-        } catch (CustomException e) {
-            System.out.println(e.getMessage());
-            Assert.assertEquals(CustomException.ExceptionType.BINDING_PROBLEM_AT_RUNTIME, e.type);
-        }
-
-    }
 
 
-    }
+    /*@Test
+    public void when_ReadstateCensusCSV_File_Count_Records_Should_Return_True() throws IOException, CustomException {
+        StateCensusAnalyser analyser = new StateCensusAnalyser("/Users/abhinavthakur/Desktop/Intellij/IndianStatesCensusAnalyzer/StateCensusData.csv");
+        System.out.println("No of Records in StateCSV: " + analyser.getStateCensusRecord());
+        Assert.assertEquals(28, analyser.getStateCensusRecord());
+    }*/
+}
+
+
 
