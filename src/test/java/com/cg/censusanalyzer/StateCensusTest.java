c308f9c07bb98206122aafca426cba3ccbc864f0
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-27 00:49:48 +0530

    Delimiter exception

--- src/test/java/com/cg/censusanalyzer/StateCensusTest.java
+++ src/test/java/com/cg/censusanalyzer/StateCensusTest.java
@@ -28,6 +28,16 @@ public class StateCensusTest {
         }
 
     }
+    @Test
+    public void when_CorrectCSVFile_But_Delimiter_Incorrect_Should_Return1_False() throws IOException {
+        try {
+            Assert.assertEquals(37, stateCensusAnalyser.getCountStateRecords("/Users/abhinavthakur/Desktop/Intellij/IndianStatesCensusAnalyzer/StateCode.csv","com.cg.censusanalyzer.State"));
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+            Assert.assertEquals(CustomException.ExceptionType.BINDING_PROBLEM_AT_RUNTIME, e.type);
+        }
+
+    }
 
 
     }
