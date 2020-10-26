1d22a9d8ad65a740483ee5d8c56087274886900c
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-27 01:13:29 +0530

    Count data

--- src/test/java/com/cg/censusanalyzer/StateCensusTest.java
+++ src/test/java/com/cg/censusanalyzer/StateCensusTest.java
@@ -8,12 +8,12 @@ import java.io.IOException;
 public class StateCensusTest {
 
 
-    /*@Test
+    @Test
     public void when_ReadstateCensusCSV_File_Count_Records_Should_Return_True() throws IOException, CustomException {
         StateCensusAnalyser analyser = new StateCensusAnalyser("/Users/abhinavthakur/Desktop/Intellij/IndianStatesCensusAnalyzer/StateCensusData.csv");
         System.out.println("No of Records in StateCSV: " + analyser.getStateCensusRecord());
         Assert.assertEquals(28, analyser.getStateCensusRecord());
-    }*/
+    }
 }
 
 
